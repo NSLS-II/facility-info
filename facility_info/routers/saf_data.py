70dce962e1935a1085c00f97f7b32518aeb3325a
Jun Aishima <jaishima@bnl.gov>
2020-10-08 13:57:41 -0400

    add another saf and connect both safs to experiment data

--- facility_info/routers/saf_data.py
+++ facility_info/routers/saf_data.py
@@ -1,3 +1,5 @@
 # {experiment_id: {experiment_id, overall_level, item1: {name, quantity, disposition, transport, safety_level}}}
 safs = {'1':{'experiment_id':2, 'overall_level':'Non-hazardous', 'items':
-      [{'name': 'Tris buffer', 'quantity':'10.0 mg', 'disposition':'user_facility', 'transport':'resident material', 'safety_level':'Non-hazardous'}]}}
+      [{'name': 'Tris buffer', 'quantity':'10.0 mg', 'disposition':'user_facility', 'transport':'resident material', 'safety_level':'Non-hazardous'}]},
+        '2':{'experiment_id':1, 'overall_level':'Non-hazardous', 'items':
+      [{'name':'Trypsin crystals', 'quantity':'100.00 mg', 'disposition':'user_facility', 'transport':'resident material', 'safety_level':'Non-hazardous'}]}}
